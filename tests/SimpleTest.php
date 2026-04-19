e109b19695480c722ef3f29e5802ecb8caf1d584
Ochikun <earlybread3044@gmail.com>
2026-04-20 00:26:48 +0900

    add: git actions environment

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function test_basic_arithmetic(): variant_mod
+    {
+        $this->assertEquals(2,1+1);
+    }
+}
